184033187de130d9a4f7b659bc7e58070c69a833
Arina Siedina <orynasiedina@gmail.com>
2023-10-13 18:17:45 +0200

    basic middlewares and routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 const dotnev = require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').default;
 const bodyParser = require('body-parser');
-
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-mongoose
-    .connect(process.env.DB_URI)
+mongoose.connect(process.env.DB_URI)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port: ${PORT}`);
         })
     }).catch(err => {
-        console.log(err);
-    })
+    console.log(err);
+})
+
+// ------MIDDLEWARES------
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// -----ROUTES------
+app.get("/", (req, res) => {
+    res.send("Home page");
+})
